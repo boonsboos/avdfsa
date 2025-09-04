02dc3c3a92ba82ab879be48f7d73c605ee2e24fd
boonsboos <mrsherobrinenaomi@gmail.com>
2025-09-05 00:31:20 +0200

    sanity check on slightly different text

--- src/test/kotlin/nl/boonsboos/test/lessontwo/OneTest.kt
+++ src/test/kotlin/nl/boonsboos/test/lessontwo/OneTest.kt
@@ -33,4 +33,30 @@ class OneTest {
         assertEquals(206, result.nrOfLetters)
         assertEquals(40, result.nrOfWords)
     }
+
+    val testText2 = """
+        Text statistieken achterhalen,
+        is met Kotlin 1.9.10 eenvoudig.
+        En natuurlijk ook in oudere versies zoals:
+        in Kotlin 1.6, Kotlin 1.7, Kotlin 1.8.20 
+        Gebruik de when { ... } om
+        met c.isWhitespace en c.isLetter(),
+        en andere handige functies te tellen
+        hoeveel witruimte, letters, ... er zijn.
+    """.trimIndent()
+
+    @Test
+    fun testGetStatistics2() {
+        // Act
+        val result = one.getStatistics(testText2)
+
+        // Assert
+        assertEquals(8, result.nrOfLines)
+        assertEquals(12, result.nrOfDigits)
+        assertEquals(testText2.length, result.textLength)
+        assertEquals(27, result.nrOfOther)
+        assertEquals(45, result.nrOfWhiteSpace)
+        assertEquals(204, result.nrOfLetters)
+        assertEquals(39, result.nrOfWords)
+    }
 }
